1900f9191534eb65479f84b5bd7ac6628e5ca228
Sunrise <nikitatropin3@gmail.com>
2023-03-10 16:54:33 +0300

    task 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function randomValue (from, to) {
+  if(from<0) from = 0; //диапазон может быть только положительный, включая ноль
+  if(from>=to) return -1; //возвращает -1 если начальное число диапазона меньше конечного
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from)) + from;
+}
 let fitLength = function(str, max_len){
   return str.length <= max_len;
 }
