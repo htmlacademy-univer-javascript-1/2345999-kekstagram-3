475649102dcd226cc5b567db69260164a7594c58
Sunrise <nikitatropin3@gmail.com>
2023-03-10 18:05:38 +0300

    eslint advices

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 function randomValue (from, to) {
-  if(from<0) from = 0; //диапазон может быть только положительный, включая ноль
-  if(from>=to) return -1; //возвращает -1 если начальное число диапазона меньше конечного
-  from = Math.ceil(from);
-  to = Math.floor(to);
-  return Math.floor(Math.random() * (to - from)) + from;
+  if (from < 0) from = 0 // диапазон может быть только положительный, включая ноль
+  if (from >= to) return -1 // возвращает -1 если начальное число диапазона меньше конечного
+  from = Math.ceil(from)
+  to = Math.floor(to)
+  return Math.floor(Math.random() * (to - from)) + from
 }
-let fitLength = function(str, max_len){
-  return str.length <= max_len;
+
+const fitLength = function (str, maxLen) {
+  return str.length <= maxLen
 }
