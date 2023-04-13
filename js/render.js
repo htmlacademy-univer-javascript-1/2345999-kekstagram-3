9ed13faeafefc3e4f2665214fb8b92d8c74c2c53
Sunrise <nikitatropin3@gmail.com>
2023-04-13 21:42:48 +0300

    9.2

--- js/render.js
+++ js/render.js
@@ -1,19 +1,19 @@
-import { generatePhotosInfo } from './data.js'
+import { generatePhotosInfo } from './data.js';
 
-const pictureTemplate = document.querySelector('#picture').content
-const pictures = document.querySelector('.pictures')
-const newFragment = new DocumentFragment()
+const pictureTemplate = document.querySelector('#picture').content;
+const pictures = document.querySelector('.pictures');
+const newFragment = new DocumentFragment();
 
 generatePhotosInfo().forEach((photo) => {
-  const template = pictureTemplate.cloneNode(true)
-  const img = template.querySelector('.picture__img')
-  const likes = template.querySelector('.picture__likes')
-  const comments = template.querySelector('.picture__comments')
-  img.src = photo.url
-  likes.textContent = photo.likes
-  comments.textContent = photo.comments
-  newFragment.appendChild(template)
-})
+  const template = pictureTemplate.cloneNode(true);
+  const img = template.querySelector('.picture__img');
+  const likes = template.querySelector('.picture__likes');
+  const comments = template.querySelector('.picture__comments');
+  img.src = photo.url;
+  likes.textContent = photo.likes;
+  comments.textContent = photo.comments;
+  newFragment.appendChild(template);
+});
 
-pictures.appendChild(newFragment)
-export { pictures }
+pictures.appendChild(newFragment);
+export { pictures };
